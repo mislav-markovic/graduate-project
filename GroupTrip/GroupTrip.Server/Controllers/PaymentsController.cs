01c234fb3c2a2ff2a67e2f825c09484afbef9faa
Mislav <mislav.markovic@avl.com>
2019-01-15 14:50:00 +0100

    WIP

---
+++ GroupTrip/GroupTrip.Server/Controllers/PaymentsController.cs
@@ -0,0 +1,24 @@
+﻿using GroupTrip.Server.DataAccess;
+using GroupTrip.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupTrip.Server.Controllers
+{
+  public class PaymentsController : Controller
+  {
+    private readonly GroupTripDAL _db;
+
+    public PaymentsController(GroupTripDAL db)
+    {
+      _db = db;
+    }
+
+    // POST: Payments/Create
+    [HttpPost]
+    [Route("api/Payments/Create")]
+    public void Create([FromBody] Payment newPayment)
+    {
+      _db.AddPayment(newPayment);
+    }
+  }
+}
--- GroupTrip/GroupTrip.Server/Controllers/PersonsController.cs
+++ GroupTrip/GroupTrip.Server/Controllers/PersonsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GroupTrip.Server.DataAccess;
 using GroupTrip.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
